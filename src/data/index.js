99a0c0ae13f0d9bf32fb9e665a8cd961b837ce4b
Maksym45 <maks.meleshko12@gmail.com>
2023-10-28 15:09:12 +0300

    all ok

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,4 @@
-import data from "./data.json";
+import data from "./users_data.json";
 
 let currentData = [];
 
@@ -11,4 +11,26 @@ export function getInitialData() {
   return currentData;
 }
 
+/*let currentData = [];
 
+function mapWithRank(row, i) {
+  return { ...row, ranking: i + 1 };
+}
+
+export async function getInitialData() {
+  try {
+    const response = await fetch('/your-backend-endpoint/data.json'); // Use your actual backend URL instead of '/your-backend-endpoint/data.json'.
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from the backend.');
+    }
+
+    const data = await response.json();
+    currentData = data.map(mapWithRank);
+    return currentData;
+  } catch (error) {
+    // Error handling, for example, you can throw or log an error.
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}*/
