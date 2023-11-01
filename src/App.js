c479a67efae087e36afd401b9817f6eb031bd5d0
Maksym45 <maks.meleshko12@gmail.com>
2023-11-01 20:49:11 +0200

    suspend on different site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./styles.css";
+import "./Auth-components/App/Auth.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Auth-components/Dashboard/Dashboard";
+import Preferences from "./Auth-components/Preferences/Preferences";
+import Leaderboard from "./Leaderboard";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/item.js
+++ src/item.js
@@ -4,7 +4,7 @@ export default function Item({ row }) {
   index++;        //place on the leaderboard
   return (
     <li className="item">
-      <span className="item__index">{index}</span>
+      <span className="item__index">{index/2}</span>
       <span className="item__nickname">{row.username}</span>
       <span className="item__score">{row.score}</span>
       <span className="item__time">{row.time}</span>
