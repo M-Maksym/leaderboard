14682f16e191a83711831e3c091f677be024b1e6
Maksym45 <maks.meleshko12@gmail.com>
2023-11-03 09:07:59 +0200

    medal png

--- src/Leaderboard/item.js
+++ src/Leaderboard/item.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Gold from './Gold.png';
+import Silver from './Silver.png';
+import Bronze from './Bronze.png';
 
 let index = 0;
 let timer;
+let Ind;
 
 export default function Item({ row }) {
   // Start timer
@@ -10,16 +14,28 @@ export default function Item({ row }) {
       index = 0;
     }, 2000); // 2 seconds
   }  
+
   index++;
+  let medalImage = "";
+  if (index / 2 === 1) {
+    medalImage = Gold;
+    Ind = <img src={medalImage} alt="Medal" className="Medal" />;
+  } else if (index / 2 === 2) {
+    medalImage = Silver;
+    Ind = <img src={medalImage} alt="Medal" className="Medal" />;
+  } else if (index / 2 === 3) {
+    medalImage = Bronze;
+    Ind = <img src={medalImage} alt="Medal" className="Medal" />;
+  } else {
+    Ind = index / 2;
+  }
 
   return (
     <li className="item">
-      <span className="item__index">{index/2}</span>
+      <span className="item__index">{Ind}</span>
       <span className="item__nickname">{row.username}</span>
       <span className="item__score">{row.score}</span>
       <span className="item__time">{row.time}</span>
-
     </li>
   );
-
 }
