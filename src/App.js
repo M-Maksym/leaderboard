e2dee9f37a656ab167f36a181a892d0d124dc9ca
Maksym45 <maks.meleshko12@gmail.com>
2023-11-08 22:55:36 +0200

    create simple account

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import "./styles.css";
 import "./Auth-components/App/Auth.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Dashboard from "./Auth-components/Dashboard/Dashboard";
 import Preferences from "./Auth-components/Preferences/Preferences";
 import useToken from './Auth-components/App/useToken';
@@ -24,21 +23,14 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/preferences" element={<Preferences />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/" element={<Leaderboard />} />
           <Route path="/account" element={<UserAccount />} />
-          <Route path="/" element={<Home />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-const Home = () => {
-  return (
-    <div>
-      <h1>Home page</h1>
-      <Link to="/leaderboard">Перейти до Leaderboard</Link>
-    </div>
-  );
-};
+
 
 export default App;
