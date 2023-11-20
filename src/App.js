f0fb75f4944f36d69dce085081a1b705d906b979
Maksym45 <maks.meleshko12@gmail.com>
2023-11-20 20:30:12 +0200

    carousel

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ import "./Auth-components/App/Auth.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Auth-components/Dashboard/Dashboard";
 import Preferences from "./Auth-components/Preferences/Preferences";
-import useToken from './Auth-components/App/useToken';
-import Login from './Auth-components/Login/Login';
 import Leaderboard from "./Leaderboard/Leaderboard";
-import UserAccount from "./Leaderboard/User_account/User_account"
+import UserAccount from "./User_account/User_account";
+import GeneralPage from "./General_page/General_page"
 
 function App() {
-  const { token, setToken } = useToken();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
   return (
     <div className="App">
       
@@ -23,9 +17,9 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/preferences" element={<Preferences />} />
-          <Route path="/" element={<Leaderboard />} />
           <Route path="/account" element={<UserAccount />} />
-          
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/" element={<GeneralPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Leaderboard/users_item.js
+++ src/Leaderboard/users_item.js
@@ -1,15 +1,17 @@
-  const actualData = await fetch(
-  `https://jsonplaceholder.typicode.com/users`  //paste URL from backend
-  ).then(response => response.json());
-
+//get JSON file from backend
+const actualData = await fetch(
+    `https://jsonplaceholder.typicode.com/users`  //paste URL from backend
+    ).then(response => response.json());
   
-let currentData = [];
-
-function mapWithRank(row, i) {
-  return { ...row, ranking: i + 1 };
-}
-
-export function getInitialData() {
-  currentData = actualData.map(mapWithRank);
-  return currentData;
-}
+    
+  let currentData = [];
+  
+  function mapWithRank(row, i) {
+    return { ...row, ranking: i + 1 };
+  }
+  
+  export function getInitialData() {
+    currentData = actualData.map(mapWithRank);
+    return currentData;
+  }
+  
